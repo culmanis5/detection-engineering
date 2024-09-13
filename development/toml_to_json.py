7f89a780077a2afd8b0b67aefe59cc8676137c0b
culmanis5 <54828733+culmanis5@users.noreply.github.com>
2024-09-13 02:26:31 -0500

    updates to detection engineering tests

--- development/toml_to_json.py
+++ development/toml_to_json.py
@@ -2,9 +2,8 @@ import requests
 import os
 import tomllib
 
-url = "https://88401b10e19d4d7994c47cf7035b1eab.us-central1.gcp.cloud.es.io:9243/api/detection_engine/rules"
+url = "https://detectionengineering101.kb.us-central1.gcp.cloud.es.io:9243/api/detection_engine/rules"
 api_key = os.environ['ELASTIC_KEY']
-
 headers = {
     'Content-Type': 'application/json;charset=UTF-8',
     'kbn-xsrf': 'true',
